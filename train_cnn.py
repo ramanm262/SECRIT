bc5f18adf2a89c1b7c5f430f5a3f6fe6dde17865
Raman Mukundan <rm1277@wildcats.unh.edu>
2023-01-11 15:10:50 -0500

    Added main training script for CNN

---
+++ train_cnn.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import datetime as dt
+from datetime import datetime
+import inspect
+import time
+import matplotlib.dates as mdates
+import random
+import pickle
+
