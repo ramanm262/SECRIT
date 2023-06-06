78f810f5157ab604d061e5db6616bc588f3cb5c7
rm1277 <rm1277@unh.edu>
2023-06-06 18:01:22 -0400

    Lots of changes
    - Hyperparameter updates (still in need of true optimization)
    - Prevent `target_scaler` from seeing y's feature names by converting to numpy arrays
    - Allowed model loading (for real this time) by saving and loading weights, instead of whole model, which is apparently not allowed for subclassed models
    - Removed some redundant imports
    - Enabled saving sec predictions to file

--- train_cnn.py
+++ train_cnn.py
@@ -1,6 +1,5 @@
 from pickle import dump, load
 
-import plotting
 from preprocessing import *
 from nn import *
 from plotting import *
@@ -18,11 +17,11 @@ iono_data_path = "/data/ramans_files/iono-feather/"
 
 # CNN hyperparameters
 time_history = 30  # Minutes of time history to train on
-epochs = 100  # Maximum number of training epochs
-early_stop_patience = 10  # Number of epochs to continue to train while validation loss does not improve
+epochs = 50  # Maximum number of training epochs
+early_stop_patience = 4  # Number of epochs to continue to train while validation loss does not improve
 conv_filters_list = [128]  # List whose elements are the number of filters in the output of the corresponding conv layer
-fc_nodes_list = [1000, 100]  # List whose elements are the number of nodes in each FC layer (NOT including output layer)
-init_lr = 1e-5  # Initial learning rate
+fc_nodes_list = [100, 50]  # List whose elements are the number of nodes in each FC layer (NOT including output layer)
+init_lr = 1e-6  # Initial learning rate
 dropout_rate = 0.2
 set_proportions = [0.7, 0.15, 0.15]  # Train, validation, and test set proportions, respectively
 
@@ -36,12 +35,12 @@ station_coords_list = [np.array([62.48, 69.1, 64.33, 57.07, 40.13, 48.52, 48.27,
                        np.array([245.52, 255.0, 263.97, 224.67, 254.77, 236.58, 242.88, 284.45, 282.63, 265.36,
                                  265.92, 248.79, 238.77, 248.05, 265.34, 263.96, 256.32, 226.7, 212.14, 291.48,
                                  247.13, 240.2, 277.7, 259.1, 246.69])]
-n_sec_lat, n_sec_lon = 4, 11
+n_sec_lat, n_sec_lon = 6, 17
 w_lon, e_lon, s_lat, n_lat = 210., 300., 35., 65.
 sec_coords_list = [np.linspace(s_lat, n_lat, n_sec_lat), np.linspace(w_lon, e_lon, n_sec_lon)]
 
 # Load the training data and prepare it in the correct format
-syear, eyear = 2008, 2012
+syear, eyear = 2008, 2019
 omni_data, sec_data, n_data, e_data = preprocess_data(syear, eyear, stations_list, station_coords_list, sec_coords_list,
                                                       omni_data_path, supermag_data_path, iono_data_path,
                                                       calculate_sec=False, proportions=set_proportions)
@@ -64,14 +63,14 @@ for dataset in [X_train_storms, X_valid_storms, X_test_storms]:
 # Also scale target data, since predictions are also sensitive to target scale
 target_scaler = MinMaxScaler()
 to_fit_target_scaler = pd.concat(y_train_storms, axis=0)
-target_scaler.fit(to_fit_target_scaler)
+target_scaler.fit(to_fit_target_scaler.to_numpy())
 del to_fit_target_scaler
 for storm_num in range(len(y_train_storms)):
-    y_train_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_train_storms[storm_num]))
+    y_train_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_train_storms[storm_num].to_numpy()))
 for storm_num in range(len(y_valid_storms)):
-    y_valid_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_valid_storms[storm_num]))
+    y_valid_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_valid_storms[storm_num].to_numpy()))
 for storm_num in range(len(y_test_storms)):
-    y_test_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_test_storms[storm_num]))
+    y_test_storms[storm_num] = pd.DataFrame(target_scaler.transform(y_test_storms[storm_num].to_numpy()))
 
 case, rmse, expv, r2, corr = [], [], [], [], []  # Initialize lists for scoring each model
 all_sec_predictions = []  # For storing predictions of each SEC on the test set
@@ -109,15 +108,15 @@ if mode == "training":
                   verbose=1, shuffle=True, epochs=epochs,
                   callbacks=[early_stop])
 
-        model.save(f"models/SECRIT_sec{sec_num}_{syear}-{eyear}.h5")
+        model.save_weights(f"models/SECRIT_sec{sec_num}_{syear}-{eyear}.h5")
 
         # Plot the training loss curve
         history = model.history  # Save the model loss history
-        plotting.plot_train_hist(history, sec_num=sec_num, syear=syear, eyear=eyear)
+        plot_train_hist(history, sec_num=sec_num, syear=syear, eyear=eyear)
 
         # Test the model on the test set
         test_predictions = model.predict(X_test)
-        all_sec_predictions.append(test_predictions)
+        all_sec_predictions.append(pd.DataFrame(test_predictions))
         ground_truth = y_test
         case.append(f"system{sec_num}")
         rmse.append(np.sqrt(mean_squared_error(ground_truth, test_predictions)))
@@ -125,7 +124,7 @@ if mode == "training":
         r2.append(r2_score(ground_truth, test_predictions))
         corr.append(np.sqrt(r2_score(ground_truth, test_predictions)))
         # Plot real vs predicted
-        plotting.plot_prediction(real=ground_truth[:4000], predicted=test_predictions[:4000], sec_num=sec_num)
+        plot_prediction(real=ground_truth.iloc[:4000], predicted=test_predictions[:4000], sec_num=sec_num)
 
         del X_train, X_valid, X_test, y_train, y_valid, y_test, this_y_train_storms, this_y_valid_storms, this_y_test_storms
 
@@ -141,11 +140,13 @@ elif mode == "loading":
         X_test = np.array(X_test)
         X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1))
 
-        model = tf.keras.models.load_model(f"models/SECRIT_sec{sec_num}_{syear}-{eyear}.h5")
+        model = CNN(conv_filters_list, fc_nodes_list, n_features=12, time_history=time_history,
+                    output_nodes=1, init_lr=init_lr, dropout_rate=dropout_rate)
+        model.load_weights(f"models/SECRIT_sec{sec_num}_{syear}-{eyear}.h5")
 
         # Test the model on the test set
         test_predictions = model.predict(X_test)
-        all_sec_predictions.append(test_predictions)
+        all_sec_predictions.append(pd.DataFrame(test_predictions))
         ground_truth = y_test
         case.append(f"system{sec_num}")
         rmse.append(np.sqrt(mean_squared_error(ground_truth, test_predictions)))
@@ -153,10 +154,16 @@ elif mode == "loading":
         r2.append(r2_score(ground_truth, test_predictions))
         corr.append(np.sqrt(r2_score(ground_truth, test_predictions)))
         # Plot real vs predicted
-        plotting.plot_prediction(real=ground_truth[:4000], predicted=test_predictions[:4000], sec_num=sec_num)
+        plot_prediction(real=ground_truth[:4000], predicted=test_predictions[:4000], sec_num=sec_num)
 
 else:
     raise ValueError("mode must be 'training' or 'loading'")
+
+print("Saving SEC coefficients to file...")
+all_sec_predictions = pd.concat(all_sec_predictions, axis=1)
+all_sec_predictions = target_scaler.inverse_transform(all_sec_predictions)
+all_sec_predictions = pd.DataFrame(all_sec_predictions)
+all_sec_predictions.to_csv("sec_predictions.csv", index=False)
 
 scores = pd.DataFrame({'case': case,
                        'rmse': rmse,
@@ -164,3 +171,5 @@ scores = pd.DataFrame({'case': case,
                        'r2': r2,
                        'corr': corr})
 scores.to_csv(f"results/cnn_scores-{syear}-{eyear}.csv", index=False)
+
+print("Done!")
